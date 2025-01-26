c1c669b14f868c69cdc25b7293c3673935ceab6c
Ratulhasanuday <ratulhasanudoy412220@gmai.com>
2025-01-26 13:04:40 +0600

    login page toaster added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,79 +3,94 @@ import Navbar from '../Shared/Navbar/Navbar';
 import { Icon } from 'react-icons-kit';
 import { eyeOff } from 'react-icons-kit/feather/eyeOff';
 import { eye } from 'react-icons-kit/feather/eye'
-import {  useContext, useState } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from '../../provider/AuthPovider';
+import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from 'react-toastify';
+
 const Login = () => {
-    const location=useLocation();
-    console.log("location in the login page",location);
-    const navigate=useNavigate();
+    const location = useLocation();
+    console.log("location in the login page", location);
+    const navigate = useNavigate();
+    const { signIn } = useContext(AuthContext);
     
-    const {signIn}=useContext(AuthContext);
-    const handlaeRegister = e => {
+    const handleLogin = e => {
         e.preventDefault();
-        const form= new FormData(e.currentTarget)
+        const form = new FormData(e.currentTarget);
         const email = form.get('email');
         const password = form.get('password');
         console.log(email, password);
-        signIn(email,password)
-        .then(result=>{
+        signIn(email, password)
+        .then(result => {
             console.log(result.user);
-            //navigate after login
-            navigate(location?.state?location.state:"/")
+            toast.success("Login successful!", { autoClose: 3000, position: "top-left" });
+            setTimeout(() => {
+                navigate(location?.state ? location.state : "/");
+            }, 3000);
         })
-        .catch(error=>{
+        .catch(error => {
             console.error(error);
-        })
+            toast.error("Login failed!", { autoClose: 3000, position: "top-left" });
+        });
     }
+
     const [password, setPassword] = useState("");
     const [type, setType] = useState('password');
     const [icon, setIcon] = useState(eyeOff);
+
     const handleToggle = () => {
         if (type === 'password') {
             setIcon(eye);
-            setType('text')
+            setType('text');
         } else {
-            setIcon(eyeOff)
-            setType('password')
+            setIcon(eyeOff);
+            setType('password');
         }
     }
+
     return (
         <div>
             <Navbar></Navbar>
             <div className="hero h-full pt-16">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl ">
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                         <h2 className="text-center text-2xl mt-4">Please Login</h2>
-                        <form onSubmit={handlaeRegister} className="card-body">
+                        <form onSubmit={handleLogin} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
                                 <input type="email" name="email" placeholder="Email" className="input input-bordered" required />
                             </div>
-                            <div className="form-control ">
+                            <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
                                 <div className="flex">
                                     <input
                                         type={type}
-                                        name="password" placeholder="password" className="input input-bordered"
+                                        name="password"
+                                        placeholder="password"
+                                        className="input input-bordered"
                                         onChange={(e) => setPassword(e.target.value)}
-                                        required />
+                                        required
+                                    />
                                     <span className="flex justify-around items-center" onClick={handleToggle}>
-                                        <Icon class="absolute mr-10" icon={icon} size={25} />
+                                        <Icon className="absolute mr-10" icon={icon} size={25} />
                                     </span>
                                 </div>
                             </div>
-                            <div className="form-control ">
+                            <div className="form-control">
                                 <button className="btn btn-primary">Login</button>
                             </div>
-                            <p className="text-sm">Don't have an account? <Link to="/register" className="font-bold text-[#745d5d]">Register</Link></p>
+                            <p className="text-sm">
+                                Don't have an account? <Link to="/register" className="font-bold text-[#745d5d]">Register</Link>
+                            </p>
                         </form>
                     </div>
                 </div>
             </div>
+            <ToastContainer />
         </div>
     );
 }
